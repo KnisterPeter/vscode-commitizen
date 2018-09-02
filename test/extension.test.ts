a1e6e7107408827f38f99c407d315fc0b7e412a2
Markus Wolf <mail@markus-wolf.de>
2018-09-02 09:09:55 +0200

    chore: use yarn and update for linter warnings

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -6,7 +6,7 @@ import * as assert from 'assert';
 suite('Extension Tests', () => {
 
     test('Something 1', () => {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
+        assert.strictEqual(-1, [1, 2, 3].indexOf(5));
+        assert.strictEqual(-1, [1, 2, 3].indexOf(0));
     });
 });
