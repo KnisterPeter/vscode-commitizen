c62dbebe0fb1c7f5c712f0ed27867a4906bf9c36
niklaspechan <niklas.pechan@gmail.com>
2023-05-30 11:12:34 +1200

    fix: fix undefined body when body skipped
    when skipping the body with cz-customizable you end up with 'undefined'
    in the commit body, this change resolves that you so end up with no body
    as expected
    
    Closes #881

--- src/extension.ts
+++ src/extension.ts
@@ -564,7 +564,7 @@ class ConventionalCommitMessage {
     const main = `${this.type}${typeof this.scope === 'string' && this.scope ?
       `(${this.scope})` : ''
       }: ${this.subject}`;
-    const body = `${this.body}`;
+    const body = `${this.body ? `${this.body}` : ''}`;
     const footer = `${this.breaking ? `BREAKING CHANGE: ${this.breaking}|` : ''}${this.messageFooter()}`;
 
     return {
